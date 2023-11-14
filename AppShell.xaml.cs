bb2805b5d8fb50a5ce8b07461c61492e92e011fb
Wilson Quinto <wilson.quinto@gizlocorp.com>
2023-11-14 10:21:19 -0500

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace RelojAppDani
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ClockViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace RelojAppDani
+{
+    public class ClockViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private DateTime _dateTime;
+        private Timer _timer;
+        public DateTime FechaHora
+        {
+            get => _dateTime;
+            set
+            {
+                if (_dateTime != value)
+                {
+                    _dateTime = value;
+                    OnPropertyChanged(); // reports this property
+                }
+            }
+        }
+        public ClockViewModel()
+        {
+            this.FechaHora = DateTime.Now;
+
+            // Update the DateTime property every second.
+            _timer = new Timer(new TimerCallback((s) => this.FechaHora = DateTime.Now),
+                               null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        }
+        ~ClockViewModel() =>
+        _timer.Dispose();
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+     PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
